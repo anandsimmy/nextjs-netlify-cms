217925bce9436ffe9625915e601d0ab7f0602203
anand simmy <anand.simmy@razorthink.com>
2021-01-22 14:33:10 +0530

    added collections and hierarchies

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import { Component } from 'react'
 
 export default class Home extends Component {
     render() {
-        let { title, cats } = attributes;
         return (
             <>
                 <Head>
