e641edae74b58c3e8bd049f6e2b7a5f88178e25c
anand simmy <anand.simmy@razorthink.com>
2021-01-22 13:22:16 +0530

    updated config

--- pages/index.js
+++ pages/index.js
@@ -11,16 +11,7 @@ export default class Home extends Component {
                     <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
                 </Head>
                 <article>
-                    <h1>{title}</h1>
-                    <HomeContent />
-                    <ul>
-                        {cats.map((cat, k) => (
-                            <li key={k}>
-                                <h2>{cat.name}</h2>
-                                <p>{cat.description}</p>
-                            </li>
-                        ))}
-                    </ul>
+                    <h1>Razorthink Help Documentation</h1>
                 </article>
             </>
         )
